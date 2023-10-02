168610476b38e2c2c0b8d844e169f7759b3f5b8f
mac <91506000+prairielandelec@users.noreply.github.com>
2023-10-01 22:26:28 -0500

    improved example

--- example.py
+++ example.py
@@ -3,12 +3,13 @@ import KCSCApi
 print()
 print()
 
-stop1 = KCSCApi.KCSCApiReturn()
-stop1.stopId = 1601
-rc = KCSCApi.getNow(stop1)
-if rc != 0:
+stop1 = KCSCApi.KCSCApiReturn() #create a new object with type KCSCApiReturn
+stop1.stopId = 1601             #set the stopId to be River Market West Southbound
+rc = KCSCApi.getNow(stop1)      #get the payload for the given stopId
+
+if rc != 0:                     #if we do not succeed, print the error and escape
     print(rc)
-else:
+else:                           #if we succeed, print all of the data in the object formatted
     print("Stop : " + stop1.stopName)
     print()
     print("Next car at: " + stop1.nextTime12)
@@ -21,6 +22,8 @@ else:
     print("----------------------------------------------")
     print()
 
+##  do the same thing, but this time get the streetcar coming after the next streetcar. This can be useful if time is too low to get to the next car
+
 stop1secondTrain = KCSCApi.KCSCApiReturn()
 stop1secondTrain.stopId = 1601
 stop1secondTrain.predictIndex = 1
@@ -28,17 +31,23 @@ rc = KCSCApi.getNow(stop1secondTrain)
 if rc != 0:
     print(rc)
 else:
-    print("Stop : " + stop1secondTrain.stopName)
-    print()
-    print("Next car at: " + stop1secondTrain.nextTime12)
-    print("            ", str(stop1secondTrain.nextTime) + " unix epoch time")
-    print("            ", str(stop1secondTrain.nextTimeDT) + " datetime")
-    print("Next car in: ", str(stop1secondTrain.nextSecs) + " seconds")
-    print()
-    print("Next car occupancy: ", stop1secondTrain.nextOccupancy, "%")
-    print()
-    print("----------------------------------------------")
-    print()
+    if stop1secondTrain.nextSecs < 120:
+        print("next car arrives at" + stop1secondTrain.stopName + " in: " + stop1.nextSecs + "seconds which is less than two minutes")
+        print("Getting next car")
+        print()
+        print("Stop : " + stop1secondTrain.stopName)
+        print()
+        print("Next car at: " + stop1secondTrain.nextTime12)
+        print("            ", str(stop1secondTrain.nextTime) + " unix epoch time")
+        print("            ", str(stop1secondTrain.nextTimeDT) + " datetime")
+        print("Next car in: ", str(stop1secondTrain.nextSecs) + " seconds")
+        print()
+        print("Next car occupancy: ", stop1secondTrain.nextOccupancy, "%")
+        print()
+        print("----------------------------------------------")
+        print()
+
+## same as above, but this time get the data for City Market Eastbound
 
 stop2 = KCSCApi.KCSCApiReturn()
 stop2.stopId = 1615
